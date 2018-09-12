790ac1baa9596a0354a3e78e5294ad6c4d0131e1
Alena Nikolaenko <lbidarka@gmail.com>
2018-09-12 16:05:54 +0300

    добавила иконки в меню, поправила отступы там же

--- source/js/script.js
+++ source/js/script.js
@@ -1,13 +1,15 @@
-"use strict";
+'use strict';
 
-var html = document.querySelector(".nojs");
-html.classList.remove("nojs");
+var html = document.querySelector('.nojs');
+html.classList.remove('nojs');
 
 // меню
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
 
 navToggle.addEventListener('click', function() {
+  // navMain.classList.toggle('main-nav--closed');
+  // navMain.classList.toggle('main-nav--opened');
   if (navMain.classList.contains('main-nav--closed')) {
     navMain.classList.remove('main-nav--closed');
     navMain.classList.add('main-nav--opened');
